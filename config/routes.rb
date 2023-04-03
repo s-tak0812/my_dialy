9f2835a61bcda01a18bc92670eebf888b7556a7c
s-tak0812 <122010689+s-tak0812@users.noreply.github.com>
2023-04-03 17:43:00 +0000

    [Save]Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,52 +1,57 @@
 Rails.application.routes.draw do
+
   namespace :admin do
     get 'contacts/index'
     get 'contacts/edit'
-  end
-  namespace :admin do
+
     get 'homes/top'
-  end
-  namespace :admin do
+
     get 'customers/index'
     get 'customers/edit'
+
+    get 'searches/search'
+
   end
+
+
+
+
   namespace :public do
     get 'contacts/new'
-  end
-  namespace :public do
+
     get 'effective_dates/index'
     get 'effective_dates/show'
-  end
-  namespace :public do
+
     get 'schedules/new'
     get 'schedules/edit'
-  end
-  namespace :public do
+
     get 'life_cycles/new'
     get 'life_cycles/edit'
-  end
-  namespace :public do
+
     get 'household_budgets/new'
     get 'household_budgets/edit'
-  end
-  namespace :public do
+
     get 'blogs/new'
     get 'blogs/edit'
     get 'blogs/show'
-  end
-  namespace :public do
+
     get 'customers/show'
     get 'customers/edit'
-  end
-  namespace :public do
+
     get 'homes/top'
     get 'homes/about'
-  end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-# 顧客用
-devise_for :customers
 
-# 管理者用
-devise_for :admin
+    get 'searches/search'
+
+  end
+
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # 顧客用
+  devise_for :customers
+
+  # 管理者用
+  devise_for :admin
 
 end
