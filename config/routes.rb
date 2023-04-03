d22d3a21c781d89d27553b7a7ee662fbe5bd54b0
s-tak0812 <122010689+s-tak0812@users.noreply.github.com>
2023-04-03 13:49:26 +0000

    [Add]gem:devise,model:customer,admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# 顧客用
+devise_for :customers
+
+# 管理者用
+devise_for :admin
+
 end
