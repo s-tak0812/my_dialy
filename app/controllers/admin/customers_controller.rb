188eabcaa1e3b304767e7f0f9f62e25b34b7e283
s-tak0812 <122010689+s-tak0812@users.noreply.github.com>
2023-04-07 12:23:43 +0000

    [Add]admin:customers

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -10,7 +10,13 @@ class Admin::CustomersController < ApplicationController
   def update
     @customer = Customer.find(params[:id])
     @customer.update(customer_params)
-    redirect_to admin_customer_path(@customer.id)
+    redirect_to admin_customers_path
+  end
+
+  def destroy
+    @customer = Customer.find(params[:id])
+    @customer.destroy
+    redirect_to admin_customers_path
   end
 
   private
