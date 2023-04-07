99ce5b6bb233fdd7e6bdf77be18513648e207a03
s-tak0812 <122010689+s-tak0812@users.noreply.github.com>
2023-04-07 11:40:12 +0000

    [Add]admin:customer_controller

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,7 +1,22 @@
 class Admin::CustomersController < ApplicationController
   def index
+    @customers = Customer.all.page(params[:page])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
+
+  def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to admin_customer_path(@customer.id)
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :email, :is_deleted, :profile_image)
+  end
+
 end
