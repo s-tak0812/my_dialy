3cf303b5e9ef5c2ad7236ff4df526be4bfc97e4a
s-tak0812 <122010689+s-tak0812@users.noreply.github.com>
2023-04-08 03:59:29 +0000

    [Add]validates

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,13 @@
 class Blog < ApplicationRecord
   belongs_to:customer, dependent: :destroy
   belongs_to:effective_date
+
+  with_options presence: true do
+    validates :customer_id
+    validates :title
+    validates :body
+  end
+
+  has_one_attached:image
+
 end
