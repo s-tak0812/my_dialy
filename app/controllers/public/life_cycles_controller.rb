ccb08bff3eececacb55500e9ac843f61ac33c1ca
s-tak0812 <122010689+s-tak0812@users.noreply.github.com>
2023-04-09 08:50:27 +0000

    [Add]life_cycle

--- app/controllers/public/life_cycles_controller.rb
+++ app/controllers/public/life_cycles_controller.rb
@@ -1,7 +1,50 @@
 class Public::LifeCyclesController < ApplicationController
+  before_action :authenticate_customer!
+  before_action :ensure_correct_customer, only:[:edit, :update, :destroy]
+
   def new
+    @life_cycle = LifeCycle.new
+  end
+
+  def create
+    @life_cycle = current_customer.life_cycles.new(life_cycle_params)
+    if @life_cycle.save
+      redirect_to customers_mypage_path, notice: "You have created successfully."
+    else
+      render :new
+    end
+
   end
 
   def edit
   end
+
+  def update
+    if @life_cycle.update(life_cycle_params)
+      redirect_to customers_mypage_path, notice: "You have updated successfully."
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @life_cycle = LifeCycle.find(params[:id])
+    @life_cycle.destroy
+    redirect_to request.referer
+  end
+
+
+  private
+
+  def life_cycle_params
+    params.require(:life_cycle).permit(:title, :start_time, :end_time, :date)
+  end
+
+  def ensure_correct_customer
+    @life_cycle = LifeCycle.find(params[:id])
+    unless @life_cycle.customer == current_customer
+      redirect_to root_path
+    end
+  end
+
 end
--- app/models/life_cycle.rb
+++ app/models/life_cycle.rb
@@ -1,3 +1,24 @@
 class LifeCycle < ApplicationRecord
   belongs_to:customer
+
+  with_options presence: true do
+    validates :title
+    validates :start_time
+    validates :end_time
+    validates :date
+  end
+
+  enum title: { others: 0, sleeping: 1, work: 2, enjoying: 3, study: 4, meal: 5 }
+
+  # validateに設定してエラーを出力させる
+  validate :end_time_cannot_be_earlier_than_start_time
+
+  private
+
+  # end_timeがstart_timeよりも早い時間で保存されないようにするメソッド
+  def end_time_cannot_be_earlier_than_start_time
+    return unless start_time && self.end_time && (self.end_time <= start_time)
+    errors.add(:end_time, "can't be earlier than start time")
+  end
+
 end
