bc3087ee2ecfb94ee01e192f6425ddbdb15d68b0
s-tak0812 <122010689+s-tak0812@users.noreply.github.com>
2023-04-09 05:37:52 +0000

    [Add]model

--- app/models/household_budget.rb
+++ app/models/household_budget.rb
@@ -1,3 +1,12 @@
 class HouseholdBudget < ApplicationRecord
   belongs_to:customer
+
+  with_options presence: true do
+    validates :title
+    validates :price
+    validates :date
+  end
+
+  enum title: { others: 0, tax: 1, food_drink: 2, entertainment: 3, utility_costs: 4, income: 5 }
+
 end
