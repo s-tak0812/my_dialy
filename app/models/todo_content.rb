070fa353cb3fe0ed21fed16152d6752ea9cbc246
s-tak0812 <122010689+s-tak0812@users.noreply.github.com>
2023-05-17 05:55:30 +0000

    [Update]model

--- app/models/todo_content.rb
+++ app/models/todo_content.rb
@@ -1,2 +1,6 @@
 class TodoContent < ApplicationRecord
+  belongs_to :customer
+  belongs_to :todo_list
+
+  validates :content, presence: true
 end
--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,2 +1,6 @@
 class TodoList < ApplicationRecord
+  belongs_to :customer
+  has_many :todo_contents, dependent: :destroy
+  
+  validates :title, presence: true
 end
